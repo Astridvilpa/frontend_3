d02327efb9ebc6094de2ad9022f3ae5ecade865f
Astrid Vilora <astridviloria54@gmail.com>
2024-07-12 01:13:08 +0200

    Se crea los estilos de galeria

--- src/pages/galeria/Galeria.jsx
+++ src/pages/galeria/Galeria.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav, Row, Col, Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import "./Galeria.css";
 
