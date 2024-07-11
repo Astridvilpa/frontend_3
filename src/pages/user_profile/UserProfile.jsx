87acd70999d38b428e1702f4c621ae7b10285562
Astrid Vilora <astridviloria54@gmail.com>
2024-07-11 18:05:26 +0200

    se realizan cambios en el userProfile

--- src/pages/user_profile/UserProfile.jsx
+++ src/pages/user_profile/UserProfile.jsx
@@ -1,9 +1,10 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown, Form, Row, Col, Card, Button, Alert } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getProfile, updateProfile } from "../../services/userCall";
+import { getProfile, updateProfile, getAllUsers, updateUserById, deleteUserById } from "../../services/userCall";
 import "./UserProfile.css";
 import { CustomInput } from "../../components/custom_input/CustomInput";
+import { BsFillPencilFill, BsFillTrash3Fill } from "react-icons/bs";
 
 // Componente de perfil de usuario
 export default function UserProfile({ isAdmin }) {
@@ -11,6 +12,14 @@ export default function UserProfile({ isAdmin }) {
   const [email, setEmail] = useState("");
   const [editing, setEditing] = useState(false);
   const [token, setToken] = useState("");
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [showUsers, setShowUsers] = useState(false);
+  const [noUsersMessage, setNoUsersMessage] = useState("");
+  const [editingUser, setEditingUser] = useState(null);
+  const [editForm, setEditForm] = useState({ first_name: "", last_name: "", email: "" });
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -30,8 +39,6 @@ export default function UserProfile({ isAdmin }) {
           const data = response.data;
           setProfileData(data);
           setEmail(data.email);
-          console.log("profileData:", data);
-          console.log("profile Data Role:", data.role?.name);
         } else {
           console.error("Error al recuperar los datos del perfil:", response.message);
         }
@@ -40,10 +47,29 @@ export default function UserProfile({ isAdmin }) {
       }
     };
 
-    if (token && !editing) {
+    const fetchUsers = async () => {
+      try {
+        const response = await getAllUsers(token);
+        if (response.success && Array.isArray(response.data)) {
+          setUsers(response.data);
+          setFilteredUsers(response.data);
+        } else {
+          console.error("Expected array of users, received:", response);
+          setError("Error al obtener usuarios: respuesta inesperada.");
+        }
+      } catch (err) {
+        console.error("Fetch error:", err);
+        setError(`Error al obtener usuarios: ${err.message}`);
+      }
+    };
+
+    if (token) {
       getProfileHandler(token);
+      if (isAdmin) {
+        fetchUsers();
+      }
     }
-  }, [editing, token, navigate]);
+  }, [editing, token, navigate, isAdmin]);
 
   const editInputHandler = (e) => {
     const { name, value } = e.target;
@@ -56,8 +82,6 @@ export default function UserProfile({ isAdmin }) {
       ...prevData,
       [name]: value,
     }));
-
-    console.log(`${name}: ${value}`);
   };
 
   const submitChanges = async () => {
@@ -73,13 +97,62 @@ export default function UserProfile({ isAdmin }) {
     }
   };
 
+  const handleEditUserClick = (user) => {
+    if (editingUser === user.id) {
+      setEditingUser(null);
+    } else {
+      setEditingUser(user.id);
+      setEditForm({ first_name: user.first_name, last_name: user.last_name, email: user.email });
+    }
+  };
+
+  const handleEditUserChange = (e) => {
+    setEditForm({ ...editForm, [e.target.name]: e.target.value });
+  };
+
+  const handleEditUserSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await updateUserById({ id: editingUser, ...editForm }, token);
+      if (response.success) {
+        const updatedUsers = users.map(user =>
+          user.id === editingUser ? { ...user, ...editForm } : user
+        );
+        setUsers(updatedUsers);
+        setFilteredUsers(updatedUsers);
+        setEditingUser(null);
+        console.log("Usuario actualizado con éxito:", response.message);
+      } else {
+        console.error("Error al actualizar el usuario:", response.message);
+      }
+    } catch (error) {
+      console.error("Error al actualizar el usuario:", error);
+    }
+  };
+
+  const handleDeleteUserClick = async (userId) => {
+    try {
+      const response = await deleteUserById(userId, token);
+      if (response.success) {
+        const updatedUsers = users.filter(user => user.id !== userId);
+        setUsers(updatedUsers);
+        setFilteredUsers(updatedUsers);
+        console.log("Usuario eliminado con éxito:", response.message);
+      } else {
+        console.error("Error al eliminar el usuario:", response.message);
+      }
+    } catch (error) {
+      console.error("Error al eliminar el usuario:", error);
+    }
+  };
+
   if (!profileData) {
     return <div>Loading....</div>;
   }
 
   const navLinksUser = (
     <>
-      <Nav.Link as={Link} to="/profile">Perfil</Nav.Link>
+      <Nav.Link as={Link} to="/profile" onClick={() => setShowUsers(false)}>Perfil</Nav.Link>
       <Nav.Link as={Link} to="/appointments">Citas</Nav.Link>
       <Nav.Link as={Link} to="/new-appointment">Nueva Cita</Nav.Link>
       <Nav.Link as={Link} to="/cartelera">Ver Servicios</Nav.Link>
@@ -90,20 +163,88 @@ export default function UserProfile({ isAdmin }) {
 
   const navLinksAdmin = (
     <>
-      <Nav.Link as={Link} to="/all-users">Ver Todos los Usuarios</Nav.Link>
+      <Nav.Link as={Link} to="/profile" onClick={() => setShowUsers(false)}>Perfil</Nav.Link>
+      <Nav.Link onClick={() => { setShowUsers(true); navigate("/admin"); }}>Ver Todos los Usuarios</Nav.Link>
       <Nav.Link as={Link} to="/appointments">Ver Citas</Nav.Link>
       <Nav.Link as={Link} to="/cartelera">Ver Servicios</Nav.Link>
       <Nav.Link as={Link} to="/artistas">Ver Artistas</Nav.Link>
     </>
   );
 
+  const filteredUserList = (
+    <div>
+      <Form className="my-4">
+        <Form.Group controlId="filter">
+          <Form.Control
+            type="text"
+            placeholder="Filtrar por email"
+            onChange={(e) => setFilter(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      {noUsersMessage && <Alert variant="info">{noUsersMessage}</Alert>}
+      <Row>
+        {filteredUsers.filter((user) =>
+          user.email.toLowerCase().includes(filter.toLowerCase())
+        ).map((user) => (
+          <Col key={user.id} md={4} className="mb-4">
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  {user.first_name} {user.last_name}
+                  <BsFillPencilFill className="ms-2" onClick={() => handleEditUserClick(user)} />
+                  <BsFillTrash3Fill className="ms-2" onClick={() => handleDeleteUserClick(user.id)} />
+                </Card.Title>
+                <Card.Text>ID: {user.id}</Card.Text>
+                {/* <Card.Text>Rol: {user.role?.name || "No asignado"}</Card.Text> */}
+                <Card.Subtitle className="mb-2 text-muted">{user.email}</Card.Subtitle>
+                {editingUser === user.id && (
+                  <Form onSubmit={handleEditUserSubmit}>
+                    <Form.Group controlId="formFirstName">
+                      <Form.Label>Nombre</Form.Label>
+                      <Form.Control 
+                        type="text" 
+                        name="first_name"
+                        value={editForm.first_name}
+                        onChange={handleEditUserChange} 
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="formLastName">
+                      <Form.Label>Apellido</Form.Label>
+                      <Form.Control 
+                        type="text" 
+                        name="last_name"
+                        value={editForm.last_name}
+                        onChange={handleEditUserChange} 
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="formEmail">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control 
+                        type="email" 
+                        name="email"
+                        value={editForm.email}
+                        onChange={handleEditUserChange} 
+                      />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                      Guardar
+                    </Button>
+                  </Form>
+                )}
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+
   return (
     <div className="profile-body">
       <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
         <Container>
-          <Navbar.Brand as={Link} to="/">
-            Tattoo Studio
-          </Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Tattoo Studio</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
@@ -125,37 +266,42 @@ export default function UserProfile({ isAdmin }) {
 
       <div className="profile-container">
         <div className="profile-content">
-          <p>Bienvenido</p>
-          <p>{profileData.role?.name || "Sin rol"}</p>
-          <CustomInput
-            type="text"
-            name="first_name"
-            value={profileData.first_name || ""}
-            handler={editInputHandler}
-            isDisabled={!editing}
-          />
-          <CustomInput
-            type="text"
-            name="last_name"
-            value={profileData.last_name || ""}
-            handler={editInputHandler}
-            isDisabled={!editing}
-          />
-          <CustomInput
-            type="email"
-            name="email"
-            value={email}
-            handler={editInputHandler}
-            isDisabled={!editing}
-          />
-          {editing ? (
+          {!showUsers && (
             <>
-              <button onClick={() => setEditing(false)}>Descartar</button>
-              <button onClick={submitChanges}>Guardar</button>
+              <p>Bienvenido</p>
+              <p>{profileData.role?.name || "Sin rol"}</p>
+              <CustomInput
+                type="text"
+                name="first_name"
+                value={profileData.first_name || ""}
+                handler={editInputHandler}
+                isDisabled={!editing}
+              />
+              <CustomInput
+                type="text"
+                name="last_name"
+                value={profileData.last_name || ""}
+                handler={editInputHandler}
+                isDisabled={!editing}
+              />
+              <CustomInput
+                type="email"
+                name="email"
+                value={email}
+                handler={editInputHandler}
+                isDisabled={!editing}
+              />
+              {editing ? (
+                <>
+                  <button onClick={() => setEditing(false)}>Descartar</button>
+                  <button onClick={submitChanges}>Guardar</button>
+                </>
+              ) : (
+                <button onClick={() => setEditing(true)}>Editar</button>
+              )}
             </>
-          ) : (
-            <button onClick={() => setEditing(true)}>Editar</button>
           )}
+          {profileData.role?.name === "super_admin" && showUsers && filteredUserList}
         </div>
       </div>
     </div>
--- src/services/userCall.js
+++ src/services/userCall.js
@@ -20,6 +20,25 @@ export const getProfile = async (token) => {
 };
 
 
+export const getAllUsers = async (token) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  try {
+    const response = await fetch(`${BASE_URL}/users`, options);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
 
 export const updateProfile = async (data, token) => {
@@ -46,3 +65,45 @@ export const updateProfile = async (data, token) => {
 
 
 
+export const updateUserById = async (data, token) => {
+  console.log(data, token, "data y token en updateUserById");
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(`${BASE_URL}/users/${data.id}`, options);
+    const responseData = await response.json();
+    console.log(responseData);
+    return responseData;
+  } catch (error) {
+    console.log(error);
+    return { success: false, message: error.message };
+  }
+};
+
+export const deleteUserById = async (id, token) => {
+  console.log(id, token, "data y token en deleteUserById");
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  try {
+    const response = await fetch(`${BASE_URL}/users/${id}`, options);
+    const responseData = await response.json();
+    console.log(responseData);
+    return responseData;
+  } catch (error) {
+    console.log(error);
+    return { success: false, message: error.message };
+  }
+};
