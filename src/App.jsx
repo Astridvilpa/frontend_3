2d37fdae469b963667d74d915cd0120935d1179a
Astrid Vilora <astridviloria54@gmail.com>
2024-07-11 10:36:47 +0200

    Se crea la pagina de home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
 
 function App() {
 
 
   return (
     <Routes>
+    <Route path='/' element= {<Home />} />
 
-      
     </Routes>
       
   )
